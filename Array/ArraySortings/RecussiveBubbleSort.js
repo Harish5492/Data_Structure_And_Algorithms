67d7cfbb5804f3a4c708411e6f69e4724d5336b6
Harish_ATR1201 <harishrana5492@gmial.com>
2025-07-31 12:20:42 +0530

    added the recurssiveBubbleSort

---
+++ Array/ArraySortings/RecussiveBubbleSort.js
@@ -0,0 +1,11 @@
+function bubbleSortByRecurrsion(arr, n) {
+  if (n === 1) return arr;
+  for (let i = 0; i < n - 1; i++) {
+    if (arr[i] > arr[i + 1]) {
+      [arr[i], arr[i + 1]] = [arr[i + 1], arr[i]];
+    }
+  }
+  return bubbleSortByRecurrsion(arr, n - 1);
+}
+
+console.log(bubbleSortByRecurrsion([64, 34, 25, 12, 22, 11, 90], 7));
